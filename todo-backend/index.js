ae38968e1d147135211111b57a04e31b7d04d798
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-08-10 14:34:27 +0545

    added origin to cors

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 //middleware
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({ origin: appConfig.CLIENT_URL }));
 
 app.use(express.urlencoded({ extended: true }));
 
